40b9b558d219768a3abc8c3d481549d755bdd7f9
kelvintechie1 <41085962+kelvintechie1@users.noreply.github.com>
2020-04-04 08:30:09 -0500

    Testing

--- tools/add.sh
+++ tools/add.sh
@@ -4,20 +4,20 @@
 NOTE=$1
 
 cd /Users/kelvintran/GithubRepos/yycnetlabs-anki/tools
-BASEPATH="/Users/kelvintran/GithubRepos/yycnetlabs-anki/CCNP-Ent_v8"
+BASEPATH="/Users/kelvintran/GithubRepos/yycnetlabs-anki/CCNP-ENCOR_v8"
 DECK="CCNP-ENCOR_v8.json"
 
-MISSINGFIELDS=$(/bin/grep -c -e '\"\"' ${BASEPATH}/${NOTE})
+MISSINGFIELDS=$(/usr/bin/grep -c -e '\"\"' ${BASEPATH}/${NOTE})
 
 if [ ${MISSINGFIELDS} -ne 0 ]; then
   echo "ERROR: ${BASEPATH}/${NOTE} is missing these non-optional fields:"
-  /bin/grep -e '\"\"' ${BASEPATH}/${NOTE}
+  /usr/bin/grep -e '\"\"' ${BASEPATH}/${NOTE}
   exit 1
 fi
 
 ruby add_note.rb -f ${BASEPATH}/${DECK} -n ${BASEPATH}/${NOTE}
 
 if [ "$?" -eq 0 ]; then
-  /bin/sed -i -e 's/\(\(front\|back\).*":"\)[^"]*\("\)/\1\3/' ${BASEPATH}/${NOTE}
+  /usr/bin/sed -i -e 's/\(\(front\|back\).*":"\)[^"]*\("\)/\1\3/' ${BASEPATH}/${NOTE}
 fi
 
